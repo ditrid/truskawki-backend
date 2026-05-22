d3bf53e4cf351a6452bfc4ec7184dc9e3f3b47f2
ditrid <48740396+ditrid@users.noreply.github.com>
2026-05-22 02:54:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -243,8 +243,6 @@ const [sprzedaz] = await pool.query(`
 });
 
 
-// Musi być process.env.PORT !
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Serwer działa poprawnie!`);
